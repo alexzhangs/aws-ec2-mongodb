57596f6d95ce4f87415d032f7297416617fed961
alex <alexzhangs@gmail.com>
2017-02-10 11:19:30 +0800

    update install.sh and readme

--- README.md
+++ README.md
@@ -1,10 +1,72 @@
-# aws-ec2-mongodb-org-3.2
+# aws-ec2-mongodb
 
-Install MongoDB Community Edition 3.2 on AWS EC2 instance
+Install Mongodb on AWS EC2 instance.
 
-## Installation
+## Get code
 
 ```
-git clone https://github.com/alexzhangs/aws-ec2-mongodb-org-3.2.git
-sudo sh aws-ec2-mongodb-org-3.2/install.sh
+sudo yum install -y git
+git clone https://github.com/alexzhangs/aws-ec2-mongodb.git
+```
+
+## Install Mongodb
+
+Mongodb is not avalaible in AWS built-in yum repo, you will need
+to provide a repo URL to this script.
+
+I provide a repo URL at
+`https://gist.github.com/alexzhangs/3eb4371c5d19b11b2c9feef458ac11b3/raw`.
+
+mongodb.repo
+
+```
+[mongodb-org-3.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/amazon/2013.03/mongodb-org/3.2/x86_64/
+gpgcheck=1
+gpgkey=https://www.mongodb.org/static/pgp/server-3.2.asc
+enabled=1
+```
+
+Please note that `gist.github.com` is unaccessable within China deal
+to well known reason.
+
+Install Mongodb from yum repo:
+
+```
+sudo sh aws-ec2-mongodb/install.sh \
+    -r https://gist.github.com/alexzhangs/3eb4371c5d19b11b2c9feef458ac11b3/raw \
+    -v org-3.2
+```
+
+In case there's other yum repo enabled, you may want to install Mongodb
+from some repo only, e.g. `mongodb-org-3.2` here, use:
+
+```
+sudo sh aws-ec2-mongodb/install.sh \
+    -r https://gist.github.com/alexzhangs/3eb4371c5d19b11b2c9feef458ac11b3/raw \
+    -n mongodb-org-3.2
+```
+
+Or you can install Mongodb from a RPM file path or URL, this is
+useful if experiencing slow network during downloading package from yum repo.
+
+from local file:
+
+```
+sudo sh aws-ec2-mongodb/install.sh \
+    -f ~/mongodb.rpm
+```
+
+from URL:
+
+```
+sudo sh aws-ec2-mongodb/install.sh \
+    -f http://somewhere.com/mongodb.rpm
+```
+
+See script help:
+
+```
+sh aws-ec2-mongodb/install.sh -h
 ```
--- install.sh
+++ install.sh
@@ -1,13 +1,80 @@
-#!/bin/bash
+#!/bin/bash -e
 
-# mongodb-org-3.2.repo
-cp -a ${0%/*}/mongodb-org-3.2.repo /etc/yum.repos.d/mongodb-org-3.2.repo || exit $?
+[[ $DEBUG -gt 0 ]] && set -x || set +x
 
-# yum
-yum install -y 'mongodb-org-3.2.*' || exit $?
+BASE_DIR="$(cd "$(dirname "$0")"; pwd)"
+PROGNAME=${0##*/}
 
-# service
-service mongod restart || exit $?
-chkconfig mongod on || exit $?
+usage () {
+    printf "Install Mongodb on AWS EC2 instance.\n\n"
+
+    printf "$PROGNAME\n"
+    printf "\t[-f RPM_PATH | RPM_URL]\n"
+    printf "\t[-r REPO_URL]\n"
+    printf "\t[-n REPO_NAME]\n"
+    printf "\t[-v VERSION]\n"
+    printf "\t[-h]\n\n"
+
+    printf "OPTIONS\n"
+    printf "\t[-f RPM_PATH | RPM_URL]\n\n"
+    printf "\tRPM package PATH or URL, -v VERSION is ignored with -f.\n\n"
+
+    printf "\t[-r REPO_URL]\n\n"
+    printf "\tRepo file URL.\n\n"
+
+    printf "\t[-n REPO_NAME]\n\n"
+    printf "\tRepo NAME will be enabled, all other repoes will be disabled.\n\n"
+
+    printf "\t[-v VERSION]\n\n"
+    printf "\tDefault version is determined by yum.\n\n"
+
+    printf "\t[-h]\n\n"
+    printf "\tThis help.\n\n"
+    exit 255
+}
+
+install_yum_repo () {
+    local url=$1
+    curl -vL -o /etc/yum.repos.d/mongodb.repo "$url"
+}
+
+package=mongodb
+
+while getopts f:r:n:v:h opt; do
+    case $opt in
+        f)
+            pkg_file=$OPTARG
+            ;;
+        r)
+            repo_file=$OPTARG
+            ;;
+        n)
+            repo_name=$OPTARG
+            ;;
+        v)
+            package=$package-$OPTARG
+            ;;
+        h|*)
+            usage
+            ;;
+    esac
+done
+
+if [[ -n $pkg_file ]]; then
+    yum install -y "$pkg_file"
+else
+    if [[ -n $repo_file ]]; then
+        install_yum_repo "$repo_file"
+    fi
+
+    if [[ -n $repo_name ]]; then
+        yum install --disablerepo=* --enablerepo=$repo_name -y "$package"
+    else
+        yum install -y "$package"
+    fi
+fi
+
+service mongod restart
+chkconfig mongod on
 
 exit
